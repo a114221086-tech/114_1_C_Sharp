b89accadc808093d52cb190615ddc7c16d88c936
a114221086-tech <a114221086@mail.shu.edu.tw>
2025-12-03 12:16:59 +0800

    Add files via upload

--- Tutorial 3-5/Change Counter/Change Counter/Form1.cs
+++ Tutorial 3-5/Change Counter/Change Counter/Form1.cs
@@ -12,37 +12,12 @@ namespace Change_Counter
 {
     public partial class Form1 : Form
     {
-        
-
+       
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void fiveCentsPictureBox_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void tenCentsPictureBox_Click(object sender, EventArgs e)
-        {
-           
-        }
-
-        private void twentyFiveCentsPictureBox_Click(object sender, EventArgs e)
-        {
-           
-        }
-
-        private void fiftyCentsPictureBox_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void exitButton_Click(object sender, EventArgs e)
-        {
-            // Close the form.
-            this.Close();
-        }
+        
     }
 }
